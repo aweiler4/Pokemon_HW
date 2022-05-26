c8a1fe7744c633b9786a041bff9f6b5fb502cdbd
aweiler4 <102678755+aweiler4@users.noreply.github.com>
2022-05-26 13:17:16 -0400

    Show view shows req.params.id

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/pokemon', (req, res) => {
     res.render('Index', {pokemon: pokemon});
 })
 
+app.get('/pokemon/:id', (req, res) => {
+    res.send(req.params.id);
+})
 
 app.listen(3000, () => {
     console.log('Listening on PORT 3000...')
